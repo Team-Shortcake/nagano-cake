dd559fa48f22807b35cefc308758ebf372a3f579
hiraitomoyuki <kotemo0605@gmail.com>
2021-08-14 14:03:09 +0000

    top,about追加しました

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,7 @@
 class HomesController < ApplicationController
     def top
-        
+        @genres = Genre.where(vailid_invalid_status: 0) # ジャンルの無効ステータスのものを除外検索
+        @items = Item.limit(8).offset(4) #　つのレコードから4つ取り出す
     end
     def about
     end
