751ce2d25d590100b07c56c20b0575b79c1d6e81
YukiNagao2020 <yuki.n0301@gmail.com>
2021-08-15 11:10:24 +0000

    first commit

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,10 @@
 class Order < ApplicationRecord
+  # 注文ステータス（0=支払待ち / 1=支払済み / 2=発送準備中 / 3=発送済み）
+ enum order_status: {
+      waiting: 0,
+      paid_up: 1,
+      in_production: 2,
+      preparing: 3,
+      shipped: 4
+  }
 end
