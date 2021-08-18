cf684d831bb9e7d4d1a855d963cde406386d470a
YukiNagao2020 <yuki.n0301@gmail.com>
2021-08-18 10:21:31 +0000

    Admin Custommers controller update

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -10,4 +10,18 @@ class Admin::CustomersController < ApplicationController
   def edit
     @customer = Customer.find(params[:id])
   end
+  
+   def update
+      @customer = Customer.find(params[:id])
+      if @customer.update(customer_params)
+        redirect_to admin_customer_path(@customer)
+      else
+        render 'edit'
+      end
+   end
+
+  private
+    def customer_params
+      params.require(:customer).permit(:last_name_kanji, :first_name_kanji, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :email, :is_user_status)
+    end
 end
